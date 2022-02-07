a6f14b883355dbdb2c7c6c402a79b33ccbf96ac0
andrea-s21 <andreasisneros21@gmail.com>
2022-02-07 12:52:28 -0700

    adds Plant model and details template

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,17 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Plant
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Hello!</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def plants_index(request):
+    plants = Plant.objects.all()
+    return render(request, 'plants/index.html', {'plants': plants})
+
+def plants_detail(request, plant_id):
+    plant = Plant.objects.get(id=plant_id)
+    return render(request, 'plants/detail.html', {'plant': plant})
